acf52cae32fcba5535ae7a4f4cdc6adaa8c99842
innocentkalu <ikalu.ik@gmail.com>
2016-08-10 23:51:42 +0000

    Recovering stylesheet.css file

--- final_project/app/log_well_view.rb
+++ final_project/app/log_well_view.rb
@@ -9,19 +9,31 @@ require_relative 'well'
     erb :index
   end
 
-  get '/display' do
-    content_type :json
-    @url = params[:url]
-    uri = URI(@url)
-    @filename = (/[^\/]*$/.match(@url))
-    File.open("#{@filename}", "w") {|f| f.write(Net::HTTP.get(uri)) }
+  get '/display/?:filename?' do
+    if params[:url]
+      content_type :html
+      @url = params[:url]
+      uri = URI(@url)
+      @filename = (/[^\/]*$/.match(@url))
+      File.open("#{@filename}", "w") {|f| f.write(Net::HTTP.get(uri)) }
+    else
+      @filename = params[:filename]
+    end
+    
     @worklas = CWLSLas.new("#{@filename}")
     @current_well = Well.new(@worklas)
     #Write to json file
     File.open("#{@filename}.json", "w") {|f| f.write(@current_well.welldata.to_json) }
     
-    @current_well.welldata.to_json
+    @current_well_welldata = @current_well.welldata.to_json
+    erb :display
+  end
 
+  post '/save_file' do
+    content_type :json
+    @filename = params[:file][:filename]
+    File.open("#{@filename}", "w") { |f| f.write(params[:file][:tempfile].read) }
+    redirect "/display/#{@filename}"
   end
 
 # end
