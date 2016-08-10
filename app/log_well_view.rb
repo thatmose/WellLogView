7cd83333afd75ba613b1d07d8bbb0b804866b115
InnocentKalu <ikalu.ik@gmail.com>
2016-08-10 17:02:46 -0600

    Update log_well_view.rb

--- app/log_well_view.rb
+++ app/log_well_view.rb
@@ -3,25 +3,41 @@ require 'las_reader'
 require 'json'
 require_relative 'well'
 
-class LogWellView < Sinatra::Base
+# class LogWellView < Sinatra::Base
 
   get '/' do
-     erb :index
+    erb :index
   end
 
-  get '/display' do
-    content_type :json
-    @url = params[:url]
-    uri = URI(@url)
-    @filename = (/[^\/]*$/.match(@url))
-    File.open("#{@filename}", "w") {|f| f.write(Net::HTTP.get(uri)) }
+  get '/display/?:filename?' do
+    if params[:url]
+      content_type :html
+      @url = params[:url]
+      uri = URI(@url)
+      @filename = (/[^\/]*$/.match(@url))
+      File.open("#{@filename}", "w") {|f| f.write(Net::HTTP.get(uri)) }
+    else
+      @filename = params[:filename]
+    end
+    
     @worklas = CWLSLas.new("#{@filename}")
     @current_well = Well.new(@worklas)
     #Write to json file
     File.open("#{@filename}.json", "w") {|f| f.write(@current_well.welldata.to_json) }
     
-    @current_well.welldata.to_json
-
+    @current_well_welldata = @current_well.welldata.to_json
+    
+    erb :display
   end
 
+  post '/save_file' do
+    content_type :json
+    @filename = params[:file][:filename]
+    File.open("#{@filename}", "w") { |f| f.write(params[:file][:tempfile].read) }
+    redirect "/display/#{@filename}"
+  end
+get '/render_file/:filename' do
+  params[:filename]
 end
+
+# end
