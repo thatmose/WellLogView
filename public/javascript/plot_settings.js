7d3f82a8c889e6db886b2b49ca8b27fab6ffe69c
thatmose <moseswanyonyi@gmail.com>
2016-08-11 17:50:30 -0600

    Added name attributes to plot settings

--- public/javascript/plot_settings.js
+++ public/javascript/plot_settings.js
@@ -10,9 +10,9 @@ $(document).ready(function() {
       defaults.cali_default(curve)
     } else if (curve == "RHOB") {
       defaults.rhob_default(curve)
-    } else if (curve == "ILD") {
+    } else if ((curve == "ILD") || (curve == 'LLD')){
       defaults.ild_default(curve)
-    } else if (curve == "ILM") {
+    } else if ((curve == "ILM") || (curve == 'LLS')){
       defaults.ilm_default(curve)
     } else if (curve == "SFL") {
       defaults.sfl_default(curve)
@@ -24,30 +24,34 @@ $(document).ready(function() {
       $("<input>").attr("type","checkbox").attr("id",curve).attr("name",curve).attr("value",curve).attr("text",curve).appendTo(div);
     
       $("<label>").attr("for",curve).text(curve).appendTo(div); 
-      var track_select = $(document.createElement('select')).attr("id","curve_track").appendTo(div);
+      var track_select = $(document.createElement('select')).attr("id","curve_track").attr("name","curve_track"+curve).appendTo(div);
       $("<option>").val("1").text("1").appendTo(track_select);
       $("<option>").val("2").text("2").appendTo(track_select);
       $("<option>").val("3").text("3").appendTo(track_select);
       $("<option>").val("4").text("4").appendTo(track_select);
 
-      var scale_type_select = $(document.createElement('select')).attr("id","scale_type").appendTo(div);
+      var scale_type_select = $(document.createElement('select')).attr("id","scale_type").attr("name","scale_type"+curve).appendTo(div);
       $("<option>").val("Linear").text("Linear").appendTo(scale_type_select);
       $("<option>").val("Log").text("Log").appendTo(scale_type_select);
 
-      $("<input>").attr("class","jscolor").attr("value","AB2567").attr("size","2").appendTo(div);
+      $("<input>").attr("class","jscolor").attr("value","AB2567").attr("size","2").attr("name","color"+curve).appendTo(div);
 
-      var line_style_select = $(document.createElement('select')).attr("id","line_type").appendTo(div);
+      var line_style_select = $(document.createElement('select')).attr("id","line_type").attr("name","line_type"+curve).appendTo(div);
       $("<option>").val("solid").text("solid").appendTo(line_style_select);
       $("<option>").val("dash").text("dash").appendTo(line_style_select);
       $("<option>").val("dotted").text("dotted").appendTo(line_style_select);
       $("<option>").val("dash-dotted").text("dash-dotted").appendTo(line_style_select);
 
-      $("<input>").attr("id","minscale").attr("value","0").attr("size","5").appendTo(div);
+      $("<input>").attr("id","minscale").attr("value","0").attr("name","minscale"+curve).attr("size","5").appendTo(div);
 
-      $("<input>").attr("id","maxscale").attr("value","100").attr("size","5").appendTo(div);
+      $("<input>").attr("id","maxscale").attr("value","100").attr("name","maxscale"+curve).attr("size","5").appendTo(div);
     }                 
    });
   $("<input>").attr("type","submit").attr("value","Make Plot").appendTo(form);
+  $( "form" ).submit(function( event ) {
+    console.log( $( this ).serializeArray() );
+    event.preventDefault();
+  });
 });
 
 $(document).ready(function() {    
