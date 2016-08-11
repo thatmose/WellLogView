cc9b6c01739df55e2dece07b83e2751f3ed77eb4
innocentkalu <ikalu.ik@gmail.com>
2016-08-11 22:51:03 +0000

    Created settings for default curves

--- public/javascript/plot_settings.js
+++ public/javascript/plot_settings.js
@@ -1,33 +1,51 @@
 $(document).ready(function() {
    curves = (Object.keys(welldata.curveinfo));     
-   var form = $(document.createElement('form')).appendTo("#tab2");
+   // var form = $(document.createElement('form')).attr("method","post").attr("id","main_form").attr("action","/display").appendTo("#tab2");
+   var form = document.getElementById("main_form");
    curves.forEach(function(curve){
-    var div = $(document.createElement('div')).attr("id","curve_settings").appendTo(form); 
-              
-    $("<input>").attr("type","checkbox").attr("id",curve).attr("name",curve).attr("value",curve).attr("text",curve).appendTo(div);
+    var div = $(document.createElement('div')).attr("id","curve_settings").appendTo(form);
+    if (curve == "GR") {
+      defaults.gr_default(curve)
+    } else if (curve == "CALI") {
+      defaults.cali_default(curve)
+    } else if (curve == "RHOB") {
+      defaults.rhob_default(curve)
+    } else if (curve == "ILD") {
+      defaults.ild_default(curve)
+    } else if (curve == "ILM") {
+      defaults.ilm_default(curve)
+    } else if (curve == "SFL") {
+      defaults.sfl_default(curve)
+    } else if ((curve == "CNS") || (curve == "NPHI")) {
+      defaults.nphi_default(curve)
+    } else if ((curve == "DT") || (curve == "DTCO")) {
+      defaults.dt_default(curve)
+    } else {
+      $("<input>").attr("type","checkbox").attr("id",curve).attr("name",curve).attr("value",curve).attr("text",curve).appendTo(div);
     
-    $("<label>").attr("for",curve).text(curve).appendTo(div); 
-    var track_select = $(document.createElement('select')).attr("id","curve_track").appendTo(div);
-    $("<option>").val("1").text("1").appendTo(track_select);
-    $("<option>").val("2").text("2").appendTo(track_select);
-    $("<option>").val("3").text("3").appendTo(track_select);
-    $("<option>").val("4").text("4").appendTo(track_select);
+      $("<label>").attr("for",curve).text(curve).appendTo(div); 
+      var track_select = $(document.createElement('select')).attr("id","curve_track").appendTo(div);
+      $("<option>").val("1").text("1").appendTo(track_select);
+      $("<option>").val("2").text("2").appendTo(track_select);
+      $("<option>").val("3").text("3").appendTo(track_select);
+      $("<option>").val("4").text("4").appendTo(track_select);
 
-    var scale_type_select = $(document.createElement('select')).attr("id","scale_type").appendTo(div);
-    $("<option>").val("Linear").text("Linear").appendTo(scale_type_select);
-    $("<option>").val("Log").text("Log").appendTo(scale_type_select);
+      var scale_type_select = $(document.createElement('select')).attr("id","scale_type").appendTo(div);
+      $("<option>").val("Linear").text("Linear").appendTo(scale_type_select);
+      $("<option>").val("Log").text("Log").appendTo(scale_type_select);
 
-    $("<input>").attr("class","jscolor").attr("value","AB2567").attr("size","2").appendTo(div);
+      $("<input>").attr("class","jscolor").attr("value","AB2567").attr("size","2").appendTo(div);
 
-    var line_style_select = $(document.createElement('select')).attr("id","line_type").appendTo(div);
-    $("<option>").val("solid").text("solid").appendTo(line_style_select);
-    $("<option>").val("dash").text("dash").appendTo(line_style_select);
-    $("<option>").val("dotted").text("dotted").appendTo(line_style_select);
-    $("<option>").val("dash-dotted").text("dash-dotted").appendTo(line_style_select);
+      var line_style_select = $(document.createElement('select')).attr("id","line_type").appendTo(div);
+      $("<option>").val("solid").text("solid").appendTo(line_style_select);
+      $("<option>").val("dash").text("dash").appendTo(line_style_select);
+      $("<option>").val("dotted").text("dotted").appendTo(line_style_select);
+      $("<option>").val("dash-dotted").text("dash-dotted").appendTo(line_style_select);
 
-    $("<input>").attr("id","minscale").attr("value","0").attr("size","3").appendTo(div);
+      $("<input>").attr("id","minscale").attr("value","0").attr("size","5").appendTo(div);
 
-    $("<input>").attr("id","maxscale").attr("value","100").attr("size","5").appendTo(div);
+      $("<input>").attr("id","maxscale").attr("value","100").attr("size","5").appendTo(div);
+    }                 
    });
   $("<input>").attr("type","submit").attr("value","Make Plot").appendTo(form);
 });
