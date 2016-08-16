7645ae830455edd6d9da72a1961cd3d370c5abc1
thatmose <moseswanyonyi@gmail.com>
2016-08-16 11:21:00 -0600

    Added documentation to data_utils.js

--- public/javascript/data_utils.js
+++ public/javascript/data_utils.js
@@ -2,15 +2,16 @@ const GRAPH_END = 0.92;
 const XAXIS_OFFSET = 0.01;
 const SUBPLOT_SPACING = [ [0, .30], [.37, .39], [.40, .66] ,[.70, 1]];
 
+// Initializes the variables storing general plot information.
 function init(){
   myData = {};
   // var plotCurves = [];
   // var plotJSON = {};
   myData.track = {
-      "1": 0,
-      "2": 0,
-      "3": 0,
-      "4": 0
+    "1": 0,
+    "2": 0,
+    "3": 0,
+    "4": 0
   };
   myData.xaxes = [];
   myData.traces = [];
@@ -34,13 +35,23 @@ function init(){
   return myData;
 }
 
-function generateLogRange(num){
+/**
+ * Converts a number to its equivalent log (base 10)
+ * @param {Number}
+ * @return {Number}
+ */
+ function generateLogRange(num){
   if (num == 0)
     num = 0.001;
   return Math.log10(num);
 }
 
-function generateTraceXAxes(index, val){
+/**
+ * Generates a Plotly friendly xaxis shorthand name for a curve based on its location in the queue and appends it to plotJSON object keeping track of plotting data
+ * @param {Number} index
+ * @param {String} val
+ */
+ function generateTraceXAxes(index, val){
   if (index == 0){
     plotJSON[val].xaxis = "x";
   } else {
@@ -48,7 +59,13 @@ function generateTraceXAxes(index, val){
   }
 }
 
-function generateTrackXAxes(index, val, track_num){
+/**
+ * Generates a Plotly friendly xaxis for a curve based on its location in the queue and appends it to myData object keeping track of plotting data
+ * @param {Number} index
+ * @param {String} val
+ * @param {Number} track_num
+ */
+ function generateTrackXAxes(index, val, track_num){
   if (index == 0){
     myData.xaxes[index] = ['xaxis', track_num, val];
   } else {
@@ -56,7 +73,12 @@ function generateTrackXAxes(index, val, track_num){
   }
 }
 
-function median(values) {
+/**
+ * Returns the median value of an array
+ * @param {Array} values
+ * @return {Number} median
+ */
+ function median(values) {
   values.sort( function(a,b) {return a - b;} );
   var half = Math.floor(values.length/2);
   if(values.length % 2)
@@ -65,11 +87,22 @@ function median(values) {
     return (values[half-1] + values[half]) / 2.0;
 }
 
-function logMiddle(values) {
+/**
+ * Returns the value corresponding to the middle between two points on a log scale
+ * @param {Array} values
+ * @return {Number} middleValue
+ */
+ function logMiddle(values) {
   return values[0] * (Math.sqrt(values[1]/values[0]));
 }
 
-function getMiddle(type, scale){
+/*
+ * Checks whether scale is log or linear and calls respective function to determine the middle point of the scale
+ * @param {String} type
+ * @param {Array} scale
+ * @return {Number}
+ */
+ function getMiddle(type, scale){
   if (type == 'log'){
     return logMiddle(scale);
   } else {
@@ -77,7 +110,10 @@ function getMiddle(type, scale){
   }
 }
 
-function generateLayoutYAxis(){
+/*
+ * Creates the yaxis for the plot. In this case it is always the depth data used for generating the yaxis. Accesses global variables.
+ */
+ function generateLayoutYAxis(){
   var depth = plotJSON['DEPTH'] || plotJSON['DEPT']
   myData.layout.yaxis = { 
     title: "Depth",
@@ -98,7 +134,12 @@ function generateLayoutYAxis(){
   }
 }
 
-function setTrackPositions(track_num, val){
+/*
+ * Sets the position of the curve on its respective subplot (track). First curve is necessary for setting overlay instructions for future curves.
+ * @param {Number} track_num
+ * @param {String} val
+ */
+ function setTrackPositions(track_num, val){
   if (typeof myData.trackPositions[track_num] === 'undefined'){
     myData.trackPositions[track_num] = [[val, 1]];
   } else {
@@ -107,25 +148,34 @@ function setTrackPositions(track_num, val){
   };
 }
 
+/*
+ * Returns the positioning instructions for the XAxes in a Plotly format for a curve based on its subplot (track) positioning
+ * @param {Number} track_num
+ * @param {String} curve_name
+ * @return {Array} [position, overlay_axis]
+ */
 function positionInstructions(track_num, curve_name){
-  pos_info = myData.trackPositions[track_num].filter(function(arr){
-    return arr[0] === curve_name;
-  });
-  var axis_gap = (1 - GRAPH_END)/myData.trackPositions[track_num].length;
-  var position = XAXIS_OFFSET + GRAPH_END + (pos_info[0][1] - 1) * axis_gap;
-    // if position on track is not 1, hence pos > 1
-    if (pos_info[0][1] !== 1){
-      var result = myData.trackPositions[track_num].filter(function(arr){
-        return arr[1] === 1;
-      });
-      myData.overlay_axis = plotJSON[result[0][0]].xaxis;
-    } else {
-      myData.overlay_axis = null;
-    }
+pos_info = myData.trackPositions[track_num].filter(function(arr){
+  return arr[0] === curve_name;
+});
+var axis_gap = (1 - GRAPH_END)/myData.trackPositions[track_num].length;
+var position = XAXIS_OFFSET + GRAPH_END + (pos_info[0][1] - 1) * axis_gap;
+  // if position on track is not 1, hence pos > 1
+  if (pos_info[0][1] !== 1){
+    var result = myData.trackPositions[track_num].filter(function(arr){
+      return arr[1] === 1;
+    });
+    myData.overlay_axis = plotJSON[result[0][0]].xaxis;
+  } else {
+    myData.overlay_axis = null;
+  }
 
-    return [position, myData.overlay_axis];
+  return [position, myData.overlay_axis];
 }
 
+/*
+ * Creates the plotly layout object for each XAxis based on user submitted input. Reads from global variables
+ */
 function generateLayoutXAxes(){
   myData.xaxes.forEach(function(val, index){
     myData.layout[val[0]] = {
@@ -134,27 +184,30 @@ function generateLayoutXAxes(){
       tickcolor: plotJSON[val[2]].color,
 
       tickvals: [plotJSON[val[2]].scale[0], getMiddle(plotJSON[val[2]].track_type, plotJSON[val[2]].scale), plotJSON[val[2]].scale[1]],
-      
+
       ticktext: [plotJSON[val[2]].scale[0], val[2], plotJSON[val[2]].scale[1]],
       tickwidth: 2,
-      // title: val[2],
-      side: 'top',
-      showline: true,
-      type: plotJSON[val[2]].track_type,
-      range: (plotJSON[val[2]].track_type == "log") ? plotJSON[val[2]].scale.map(generateLogRange) : plotJSON[val[2]].scale,
-      domain: generateSubplotSpacing(val[1]),
-      showgrid: true,
-      gridwidth: 2,
-      zeroline: false
-    };
-    if (positionInstructions(val[1], val[2])[1] !== null ){
-      myData.layout[val[0]].overlaying = positionInstructions(val[1], val[2])[1];
-      myData.layout[val[0]].showgrid = false;
-      myData.layout[val[0]].position = positionInstructions(val[1], val[2])[0]
-    };
-  })
+    // title: val[2],
+    side: 'top',
+    showline: true,
+    type: plotJSON[val[2]].track_type,
+    range: (plotJSON[val[2]].track_type == "log") ? plotJSON[val[2]].scale.map(generateLogRange) : plotJSON[val[2]].scale,
+    domain: generateSubplotSpacing(val[1]),
+    showgrid: true,
+    gridwidth: 2,
+    zeroline: false
+  };
+  if (positionInstructions(val[1], val[2])[1] !== null ){
+    myData.layout[val[0]].overlaying = positionInstructions(val[1], val[2])[1];
+    myData.layout[val[0]].showgrid = false;
+    myData.layout[val[0]].position = positionInstructions(val[1], val[2])[0]
+  };
+})
 }
 
+/*
+ * Generates the plotly trace object for each curve selected by the user. Reads and sets data from global variables.
+ */
 function generateTraces(){
   plotCurves.forEach(function(val, index){
     myData.traces[index] = {
@@ -172,11 +225,20 @@ function generateTraces(){
   });
 }
 
+/*
+ * Returns the spacing information in Plotly format(layout of the subplots) based on the track provided
+ * @param {Numer} track
+ * @return {Array} [domain_start, domain_end]
+ */
 function generateSubplotSpacing(track){
   var track_index = parseFloat(track) - 1;
   return SUBPLOT_SPACING[track_index];
 }
 
+/*
+ * Reads user input on display preferences and prepares Plotly format objects to be used to generate plots 
+ * @param {Object} plotObj
+ */
 function prepForPlot(plotObj){
   init();
   Object.keys(plotObj).forEach(function(val, index){
